025086a366dfbf28d90ce9bee6d8b6c123d1fb03
sanjay9 <sanjaymahabir9@gmail.com>
2023-04-21 09:54:23 -0400

    url

--- FrontEnd/src/apolloClient.js
+++ FrontEnd/src/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'https://biographql.onrender.com/graphql', // Make sure this is the correct server endpoint
+  uri: 'https://biographql.onrender.com', // Make sure this is the correct server endpoint
 });
 
 const authLink = setContext((_, { headers }) => {
